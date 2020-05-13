965f095d3ce8fa758c457f9ba1e2e83315cb48d6
Joel <joeldmyers@gmail.com>
2020-05-12 20:48:47 -0400

    first example coded

--- src/01-problem-solving-patterns/frequency-counter.js
+++ src/01-problem-solving-patterns/frequency-counter.js
@@ -1,6 +1,29 @@
 const same = (arr1, arr2) => {
-  // it should take in two arrays and check whether they
-  return false;
+  // INPUT: Two arrays of numbers. One is first one, second is squares.
+
+  let frequencyCounter = {};
+  arr1.forEach((num) => {
+    const square = num * num;
+    frequencyCounter[square] = frequencyCounter[square]
+      ? frequencyCounter[square] + 1
+      : 1;
+  });
+
+  arr2.forEach((num) => {
+    if (!frequencyCounter[num] || frequencyCounter[num] === 0) {
+      return false;
+    }
+    frequencyCounter[num] = frequencyCounter[num] - 1;
+  });
+
+  const leftoverValues = Object.values(frequencyCounter).reduce(
+    (sum, frequency) => frequency + sum,
+    0
+  );
+  if (leftoverValues > 0) return false;
+
+  // OUTPUT: should check if each value in arr1 has a square in arr2, with the same frequency.
+  return true;
 };
 
 module.exports = { same };
--- src/01-problem-solving-patterns/frequency-counter.test.js
+++ src/01-problem-solving-patterns/frequency-counter.test.js
@@ -1,8 +1,10 @@
 const { same } = require("./frequency-counter");
 
 describe("Checks Whether the same function returns true if each value in the first array has the same value in the second array", () => {
-  test("it should filter by a search term (link)", () => {
+  test("it should correctly compare the first array's numbers squared to the second array", () => {
     // actual test
     expect(same([1, 2, 3], [4, 1, 9])).toBe(true);
+    expect(same([1, 2, 3], [1, 9])).toBe(false);
+    expect(same([1, 2, 1], [4, 4, 1])).toBe(false); // must be same frequency
   });
 });
